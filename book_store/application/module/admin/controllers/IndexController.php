cb7ebc10291ebafb190c4f64208bea189c2e6e1c
Thanh Nguyen <ngtanthanh90@gmail.com>
2016-03-21 17:21:11 +0700

    Training 21/03/2016

--- book_store/application/module/admin/controllers/IndexController.php
+++ book_store/application/module/admin/controllers/IndexController.php
@@ -1,14 +1,42 @@
 <?php
 class IndexController extends Controller{
-	public function __construct(){
-	
+	public function __construct($arrParams){
+		parent::__construct($arrParams);
 	}
 	
-	public function indexAction(){
+	public function loginAction() {
+		$this->_templateObj->setFolderTemplate('admin/main/');
+		$this->_templateObj->setFileTemplate('login.php');
+		$this->_templateObj->setFileConfig('template.ini');
+		$this->_templateObj->load();
+		$this->_view->_title   =   'Login';
+		if(@$this->_arrParam['form']['token'] > 0) {
+			$validate 	=	new Validate($this->_arrParam['form']);
+			$username 	=	@$this->_arrParam['form']['username'];
+			$password 	=	@$this->_arrParam['form']['password'];
+			$query 		=	"SELECT `id` FROM `user` WHERE `username` = '{$username}' AND `password` = '{$password}'";
+			$validate->addRule('username', 'existRecord', array('database' => $this->_model, 'query' => $query));
+			$validate->run();
+			
+			if($validate->isValid() == true) {
+				
+			} else {
+				$this->_view->errors 	=	$validate->showErrors();	
+			}
+		}
+		$this->_view->render('index/login', true);
 	}
 	
-	public function addAction(){
-		echo '<h3>' . __METHOD__ . '</h3>';
+	public function indexAction() {
+		$this->_templateObj->setFolderTemplate('admin/main/');
+		$this->_templateObj->setFileTemplate('index.php');
+		$this->_templateObj->setFileConfig('template.ini');
+		$this->_templateObj->load();	
+		
+		$this->_view->_title   =   'Index';
+		
+		
+		
 		$this->_view->render('index/index');
 	}
 }
