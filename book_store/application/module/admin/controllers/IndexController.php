a0853046c6ad66fb9ec9b47cd71ddc6993012969
Thanh Nguyen <ngtanthanh90@gmail.com>
2016-03-24 16:15:56 +0700

    Login Admin P2 240316

--- book_store/application/module/admin/controllers/IndexController.php
+++ book_store/application/module/admin/controllers/IndexController.php
@@ -11,15 +11,25 @@ class IndexController extends Controller{
 		$this->_templateObj->load();
 		$this->_view->_title   =   'Login';
 		if(@$this->_arrParam['form']['token'] > 0) {
+			
 			$validate 	=	new Validate($this->_arrParam['form']);
 			$username 	=	@$this->_arrParam['form']['username'];
-			$password 	=	@$this->_arrParam['form']['password'];
+			$password 	=	md5(@$this->_arrParam['form']['passwd']);
 			$query 		=	"SELECT `id` FROM `user` WHERE `username` = '{$username}' AND `password` = '{$password}'";
+		
 			$validate->addRule('username', 'existRecord', array('database' => $this->_model, 'query' => $query));
 			$validate->run();
 			
 			if($validate->isValid() == true) {
-				
+				$infoUser 		=	$this->_model->infoItem($this->_arrParam);
+				$arraySession 	=	array(
+											'login' 		=>		true,
+											'info'			=>		$infoUser,
+											'time' 			=>		time(),
+											'group_acp' 	=>		$infoUser['group_acp']
+										);
+				Session::set('user', $arraySession);
+				URL::redirect('admin', 'index', 'index');
 			} else {
 				$this->_view->errors 	=	$validate->showErrors();	
 			}
@@ -35,7 +45,9 @@ class IndexController extends Controller{
 		
 		$this->_view->_title   =   'Index';
 		
-		
+		echo '<pre>';
+		print_r($_SESSION);
+		echo '</pre>';
 		
 		$this->_view->render('index/index');
 	}
